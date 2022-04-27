4e21cffe96540136028fbdce9f36dcc4dfae78f5
Rutheeshreddy <90040567+Rutheeshreddy@users.noreply.github.com>
2022-04-27 08:54:12 +0530

    Add files via upload

--- Assignment-3/Codes/assig3.py
+++ Assignment-3/Codes/assig3.py
@@ -2,33 +2,27 @@ import numpy as np
 import pandas as pd
 
 df = pd.read_excel('assig3.xlsx')
-data=np.array(df)
-print(data)
-C0 = [data[i][1]  for i in range(0,5,1)]
-C1 = [data[i][2]  for i in range(0,5,1)]
-C2 = [data[i][3]  for i in range(0,5,1)]
-C3 = [data[i][4]  for i in range(0,5,1)]
-
-N=sum(C0)+sum(C1)+sum(C2)+sum(C3) 
+cols_sum = df.iloc[:,1:5].sum(axis=0)
+N=sum(cols_sum) 
 print('Total number of families is {}'.format(N))
 
-p1 = data[2][3]/N 
+p1 = df.iat[2,3]/N 
 print('Probability of families earning Rs.10000 – 13000 per \
  month and owning 2 vehicles is {0:.3f}'.format(p1))
 
-p2 = data[4][2]/N
+p2 = df.iat[4,2]/N
 print('Probability of families earning Rs.16000 or more per \
  month and owning 1 vehicle is {0:.3f}'.format(p2))
 
-p3 = data[0][1]/N
+p3 = df.iat[0,1]/N
 print('Probability of families earning Rs.7000 or less per \
  month and owning no vehicle is {0:.4f}'.format(p3))
 
-p4 = data[3][4]/N
+p4 = df.iat[3,4]/N
 print('Probability of families earning Rs.13000 – 16000 per \
  month and owning more than 2 vehicles is {0:.4f}'.format(p4))
 
-n=sum(C0)+sum(C1)
+partial_sum=df.iloc[:,1:3].sum(axis=0)
+n=sum(partial_sum)
 p5=n/N
 print('Probability of families owning not more than 1 vehicle is {0:.3f}'.format(p5))
-
