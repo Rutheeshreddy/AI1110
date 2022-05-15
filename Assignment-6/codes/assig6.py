3bdab18028c052cc811ac2d3eec532479630dba0
Rutheeshreddy <90040567+Rutheeshreddy@users.noreply.github.com>
2022-05-15 09:35:08 +0530

    Add files via upload

--- Assignment-6/codes/assig6.py
+++ Assignment-6/codes/assig6.py
@@ -1,20 +1,17 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.stats import bernoulli
-from functions import *
 
 
-#Calculating probability using bernoulli simulation
+def total_prob(p1,p2,p_red1,p_red2) :
+    return p1*p_red1+p2*p_red2
 
 
 #Probability of picking red ball
-p_1= bernoulli_success_prob(0.5)
+p_1= 0.5
 #Probability of picking black ball
 p_2= 1-p_1
 #Probability of getting red ball if first ball is red
-p_red_1= bernoulli_success_prob(7/12)
+p_red_1= 7/12
 #Probability of getting red ball if first ball is black
-p_red_2= bernoulli_success_prob(5/12)
+p_red_2= 5/12
 
 #Probability that a red ball is picked
 prob= total_prob(p_1,p_2,p_red_1, p_red_2)
