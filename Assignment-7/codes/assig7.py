ba6f06d9eb1391818bc26b583ef9f6b59946ad40
Rutheeshreddy <90040567+Rutheeshreddy@users.noreply.github.com>
2022-05-20 10:20:12 +0530

    Add files via upload

--- Assignment-7/codes/assig7.py
+++ Assignment-7/codes/assig7.py
@@ -2,4 +2,4 @@ from scipy.stats import binom
 
 x=binom.pmf(4,5,0.8)
 
-print("Theoretical probability is 0.4096. Practical probability is {0:.3f}".format(x))
+print("Required probability is {0:.3f}".format(x))
